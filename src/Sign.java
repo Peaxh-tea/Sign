485dd103aa2be7ed27ded49fbc0bb5c494a7d8d7
AustinArvelo <austina46@nycstudents.net>
2025-12-15 13:17:48 -0500

    created sign class

--- src/Sign.java
+++ src/Sign.java
@@ -1,2 +1,14 @@
 public class Sign {
+    private String text = "";
+    private int width = 0;
+    public Sign(String text, int width) {
+        this.text = text;
+        this.width = width;
+        for (int i = 0; i < text.length(); i+=width) {
+            String[] lines = text.substring(i,i + width);
+        }
+    }
+    public int getLines(){
+        return lines.length();
+    }
 }
