a1a212a0b2071e4cc81013b88c2ae8fb0b9285a2
AustinArvelo <austina46@nycstudents.net>
2025-12-17 13:19:55 -0500

    Sign class

--- src/Sign.java
+++ src/Sign.java
@@ -1,14 +1,29 @@
 public class Sign {
     private String text = "";
     private int width = 0;
+    private int count = 1;
     public Sign(String text, int width) {
         this.text = text;
         this.width = width;
-        for (int i = 0; i < text.length(); i+=width) {
-            String[] lines = text.substring(i,i + width);
+        while (count <= text.length()) {
+            if (text.substring(count - 1, count).compareTo(text.substring(count, count + 1)) <= 25)
+        }
+        String[] lines = new String[count];
+        if (!this.text.isEmpty()) {
+            for (int i = 0; i < this.text.length(); i += width)
+                lines[i] = this.text.substring(i, i + width);
         }
     }
-    public int getLines(){
-        return lines.length();
+    public String getLines(){
+        String temp = "";
+        if (text.isEmpty()) return null;
+        for(int i = 0; i <= count; i++){
+            temp = text
+        }
+    }
+    public int numberOfLine(){
+        if (!text.isEmpty()) return count;
+        return 0;
     }
 }
+
