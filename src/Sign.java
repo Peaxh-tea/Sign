0e987a8e25fc764e6a9154592205752ba08f34b3
AustinArvelo <austina46@nycstudents.net>
2025-12-19 13:18:29 -0500

    added test cases, sign still needs to be appended

--- src/Sign.java
+++ src/Sign.java
@@ -5,23 +5,23 @@ public class Sign {
     public Sign(String text, int width) {
         this.text = text;
         this.width = width;
-        while (count <= text.length()) {
-            if (text.substring(count - 1, count).compareTo(text.substring(count, count + 1)) <= 25)
-        }
-        String[] lines = new String[count];
-        if (!this.text.isEmpty()) {
-            for (int i = 0; i < this.text.length(); i += width)
-                lines[i] = this.text.substring(i, i + width);
-        }
+        if (text.length() % width > 0) count = text.length()/width + 1;
+        else count = text.length()/width;
     }
     public String getLines(){
         String temp = "";
         if (text.isEmpty()) return null;
-        for(int i = 0; i <= count; i++){
-            temp = text
+        for(int i = 0; i <= count; i+= width){
+            if (i + width <= text.length()){
+                temp = temp + text.substring(i,i + width) + ";";
+            } else{
+                temp = temp + text.substring(i) + ";";
+            }
         }
+        temp = temp.substring(0, temp.length() - 1);
+        return temp;
     }
-    public int numberOfLine(){
+    public int numberOfLines(){
         if (!text.isEmpty()) return count;
         return 0;
     }
